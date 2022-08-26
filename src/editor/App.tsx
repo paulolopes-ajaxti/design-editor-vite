1bfb6782ecdc270a5f048799a535b62bd31e4ef6
paulolopes-ajaxti <paulo.junior@ajaxti.com.br>
2022-08-26 06:49:34 -0300

    first commit

---
+++ src/editor/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { ImageMapEditor } from './editors';
+
+const App = () => {
+	return (
+		<div className="rde-main">
+			<div className="rde-content">
+				<ImageMapEditor />
+			</div>
+		</div>
+	);
+};
+
+export default App;
